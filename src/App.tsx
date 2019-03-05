ef87a613412051e2b70aa2d50b597175b99c1c02
Sam Jones <sam@decisions.com>
2019-03-05 09:36:32 -0500

    css refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import logo from './agileLogoTrans.png';
-import './App.css';
-import InstrumentForm from './components/InstrumentForm';
+import React, { Component } from "react";
+import logo from "./agileLogoTrans.png";
+import "./App.css";
+import InstrumentForm from "./components/InstrumentForm";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Musical Instrument Insurance</h1>
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="mii-react-app">
+        <header className="mii-react-app-header">
+          <h1 className="mii-react-app-title">Musical Instrument Insurance</h1>
+          <img src={logo} className="mii-react-app-logo" alt="logo" />
         </header>
-        <section className="App-content">
+        <section className="mii-react-app-content">
           <InstrumentForm />
         </section>
       </div>
